c53d94b6e1752f6f08b8b4c3e91aaab8e22f1d86
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2022-10-16 16:55:17 +0530

    Time: 78 ms (10.38%), Space: 42 MB (57.45%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -1,39 +1,45 @@
 class Solution {
-
-    public int minDifficulty(int[] k, int d) {
-        if (d > k.length) return -1;
-
-        //we now have a cache!
-        //don't really need a row for the last day
-        int[][] cache = new int[d - 1][k.length];
-        for (int[] day : cache) Arrays.fill(day, -1);
-
-        return dfs(k, d - 1, 0, cache);
+    public int minDifficulty(int[] jobDifficulty, int d) {
+      if(jobDifficulty.length < d) return -1;
+       
+       int[][] dp = new int[d+1][jobDifficulty.length];
+        for(int[] row : dp)
+        {
+            Arrays.fill(row,-1);
+        }
+        
+        
+      return dfs(jobDifficulty,dp,d,0);
     }
-
-    private int dfs(int[] k, int d, int pos, int[][] cache) {
+    
+    public int dfs(int[] jobDifficulty,int[][] dp ,int d , int index)
+    {
         //base case
-        if (d == 0) {
-            int max = k[pos];
-            for (int i = pos; i < k.length; i++) max = Math.max(max, k[i]);
+        if(d == 1)
+        {
+           int max = 0;
+           while(index < jobDifficulty.length)
+           {
+               max = Math.max(max,jobDifficulty[index++]);
+           }
             return max;
         }
-
-        //just making sure we start indexing from 0
-        int day = cache.length - d;
-
-        //we already have this in the cache, just return it right here
-        if (cache[day][pos] != -1) return cache[day][pos];
-
-        // same logic as naive recursion
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = pos; i < k.length - d; i++) {
-            max = Math.max(max, k[i]);
-            min = Math.min(min, max + dfs(k, d - 1, i + 1, cache));
+        
+        if(dp[d][index]!=-1)
+        {
+            return dp[d][index];
         }
-
-        // but now we're also updating the cache the first time we calculate this
-        return cache[day][pos] = min;
+        
+        
+        int leftMax = 0;
+        
+        int res = Integer.MAX_VALUE;
+        for(int i=index;i<jobDifficulty.length-d+1;i++)
+        {
+            leftMax = Math.max(leftMax,jobDifficulty[i]);
+            res = Math.min(res,leftMax+dfs(jobDifficulty,dp,d-1,i+1));
+        }
+        
+        return dp[d][index] = res;
     }
 }
